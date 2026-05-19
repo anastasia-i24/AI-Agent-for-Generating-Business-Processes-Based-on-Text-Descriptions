318d6903f8d5495f1f469f6fe7c4ccd3bf787860
AlekseevMD <mdalekseev@edu.hse.ru>
2026-05-19 19:09:38 +0300

    Update gpd.py

--- gpd.py
+++ gpd.py
@@ -1,9 +1,16 @@
 import xml.etree.cElementTree as ET
 from beautiful_xml import indent
 
+
+def get_process_name(data):
+    if len(data) > 6 and data[6]:
+        return str(data[6])
+    return "result"
+
+
 def generate_gpd(data):
     root = ET.Element("process-diagram", {
-        "name": "result",
+        "name": get_process_name(data),
         "notation": "bpmn",
         "rendered": "graphiti",
         "showActions": "false",
@@ -12,7 +19,6 @@ def generate_gpd(data):
         "height": "653"
     })
 
-    # cоздание нод начала и конца
     for task in data[3]:
         task_name, coordinates, lane_name, id = task
         x, y = coordinates
@@ -31,7 +37,6 @@ def generate_gpd(data):
                     "name": i[1]
                 })
 
-    # создание нод usertask
     for task in data[0]:
         task_name, coordinates, lane_name, id = task
         x, y = coordinates
@@ -50,8 +55,6 @@ def generate_gpd(data):
                     "name": i[1]
                 })
 
-
-    # создание нод scripttask
     for task in data[1]:
         task_name, coordinates, id = task
         x, y = coordinates
@@ -72,14 +75,13 @@ def generate_gpd(data):
 
     return root
 
+
 def gpd_builder(data):
     filename = "gpd"
     root = generate_gpd(data)
-    tree = ET.ElementTree(root)
     indent(root)
-    xml_str = ET.tostring(tree.getroot(), encoding='unicode', method='xml')
+    xml_str = ET.tostring(root, encoding='unicode', method='xml')
     xml_str = xml_str.replace(' />', '/>')
-    with open(f"./{filename}.xml", "w") as f:
+    with open(f"./{filename}.xml", "w", encoding="utf-8") as f:
         f.write('<?xml version="1.0" encoding="UTF-8"?>\n\n')
         f.write(xml_str)
-        f.close()
