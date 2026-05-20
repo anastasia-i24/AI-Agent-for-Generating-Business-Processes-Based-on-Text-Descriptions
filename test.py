66f9a51a484092556ba94c09398ce8fe1e00ac97
Anastasiya <visnakovan055@gmail.com>
2026-05-20 18:04:36 +0300

    Удален test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-a = [
-    {'name': 'pupa', 'id': 'ID1'},
-    {'name': 'zalupa', 'id': 'ID2'},
-    {'name': 'biba', 'id': 'ID2'}
-]
-b = {}
-for i in a:
-    b[i['id'][2:]] = b.get(i['id'], []) + [i['name']]
-print(b)
